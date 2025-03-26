cecabb77cc5a26bca79f4217cf460a02d09986da
yosefgue <yosefgue6@gmail.com>
2025-03-26 03:54:39 +0000

    new button and new pages

---
+++ public/assets/fornew.js
@@ -0,0 +1,4 @@
+const selectclose = document.querySelector(".new_text button")
+selectclose.addEventListener("click", (e) => {
+    window.history.back();
+})
--- server.js
+++ server.js
@@ -5,21 +5,33 @@ app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 
+
 app.get("/", (req, res) => {
-    res.render("home", {pageClass: "active_home"});
+    res.render("home", {pageClass: "home"});
 })
 
 app.get("/clients", (req, res) => {
-    res.render("clients", {pageClass: "active_clients"});
+    res.render("clients", {pageClass: "clients", });
 })
 
 app.get("/devis", (req, res) => {
-    res.render("devis", {pageClass: "active_devis"});
+    res.render("devis", {pageClass: "devis"});
 })
 
 app.get("/factures", (req, res) => {
-    res.render("factures", {pageClass: "active_factures"});
+    res.render("factures", {pageClass: "factures"});
 })
 
+app.get("/devis/new_devis", (req, res) => {
+    res.render("new_devis", {pageClass: "new_devis"});
+})
+
+app.get("/factures/new_factures", (req, res) => {
+    res.render("new_factures", {pageClass: "new_factures"});
+})
+
+app.get("/clients/new_clients", (req, res) => {
+    res.render("new_clients", {pageClass: "new_clients"});
+})
 
 app.listen(3000);
